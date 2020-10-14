f8abce6f433a35810b3b0e066bad99879b3d40ae
taroteeth <zarahsikora@gmail.com>
2020-10-14 00:09:43 -0400

    Remove bg color

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from "../components/image"
 
 export default function Home() {
   return (
-    <div style={{ backgroundColor : `pink` }}>
+    <div>
       <h1>WELCOME TO HELL</h1>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
