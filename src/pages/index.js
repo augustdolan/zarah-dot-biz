e97a41e03f816e5f8c9cf1e050415ec260dda249
taroteeth <zarahsikora@gmail.com>
2020-10-14 00:04:55 -0400

    Change div color

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from "../components/image"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+    <div style={{ color: `#fff` }}>
       <h1>WELCOME TO HELL</h1>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
