f3143883338ff78963429cfa88a51e751192b37d
taroteeth <zarahsikora@gmail.com>
2020-10-14 10:45:15 -0400

    Experiment w bg in js vs css

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1>WELCOME TO HELL</h1>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, backgroundColor: `greenyellow` }}>
         <Image />
       </div>
     </div>
