e2d17601e79cb6afa27464e14b10e5578462e310
taroteeth <zarahsikora@gmail.com>
2020-10-14 00:03:52 -0400

    Change homepage layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout style={{ color: `purple` }}>
-    <SEO title="Home" />
-    <h1>WELCOME TO HELL</h1>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>WELCOME TO HELL</h1>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
     </div>
-  </Layout>
-)
-
-export default IndexPage
+  )
+}
