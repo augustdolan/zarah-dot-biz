88d5916f8424c5b378dad864320049dab442eadb
taroteeth <zarahsikora@gmail.com>
2020-10-14 10:47:22 -0400

    remove bg

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1>WELCOME TO HELL</h1>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, backgroundColor: `greenyellow` }}>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
     </div>
